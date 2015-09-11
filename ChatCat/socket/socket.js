d855d46969dc2d6275f92c95bff9a6f080b4313f
Li Zhang <ycy828f@gmail.com>
2015-09-11 11:08:47 -0700

    lz - chat-cat ready to deploy

--- ChatCat/socket/socket.js
+++ ChatCat/socket/socket.js
@@ -7,6 +7,39 @@ module.exports = function (io, rooms) {
       rooms.push(data);
       socket.broadcast.emit('roomupdate', JSON.stringify(rooms));
       socket.emit('roomupdate', JSON.stringify(rooms));
-    })
-  })
+    });
+  });
+
+  var message = io.of('/messages').on('connection', function (socket) {
+    console.log('Connected to the Chatroom !!');
+
+    socket.on('joinroom', function (data) {
+      socket.username = data.user;
+      socket.userPic = data.userPic;
+      socket.join(data.room);
+      updateUserList(data.room, true);
+    });
+
+    socket.on('newMessage', function (data) {
+      socket.broadcast.to(data.room_number).emit('messagefeed', JSON.stringify(data));
+    });
+
+    function updateUserList(room, updateAll) {
+      var getUsers = io.of('/messages').clients(room);
+      var userlist = [];
+
+      for (var i in getUsers) {
+        userlist.push({user:getUsers[i].username, userPic:getUsers[i].userPic});
+      }
+      socket.to(room).emit('updateUsersList', JSON.stringify(userlist));
+
+      if (updateAll) {
+        socket.broadcast.to(room).emit('updateUsersList', JSON.stringify(userlist));
+      }
+    }
+
+    socket.on('updateList', function (data) {
+      updateUserList(data.room);
+    });
+  });
 }
