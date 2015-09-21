8641e6e7c37ada3c010dfa2c07e3e344c61d3b0d
lizhang <ycy828f@gmail.com>
2015-09-20 22:21:57 -0700

    lz - set up photogrid s3

--- photogrid/app.js
+++ photogrid/app.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var path = require('path');
+var config = require('./config/config.js');
+var knox = require('knox');
+var fs = require('fs');
+var os = require('os');
+var formidable = require('formidable');
 
 var app = express();
 
@@ -9,8 +14,15 @@ app.set('view engine', 'html');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('port', (process.env.PORT || 9000));
+app.set('host', config.host);
 
-require('./routes/routes.js')(express, app);
+var knoxClient = knox.createClient({
+  key: config.S3AccessKey,
+  secret: config.S3Secret,
+  bucket: config.S3Bucket
+});
+
+require('./routes/routes.js')(express, app, formidable, fs, os);
 
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
